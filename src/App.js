c314d3daae223a48ad82ff6678907756647cf65f
Luca Fabbozzo <lucafabbozzo@gmail.com>
2022-06-04 09:00:49 -0500

    initial state and loading images

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    products: [
+      { name: 'Tomatoes', price: 1500, img: '/products/tomatoes.jpg' },
+      { name: 'Peas', price: 2500, img: '/products/peas.jpg' },
+      { name: 'Lettuce', price: 500, img: '/products/lettuce.jpg' },
+    ]
+  }
   render() {
     return(
       <div>
