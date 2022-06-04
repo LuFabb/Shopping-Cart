9493e0764ea46d4a720b6125cde194b9d0806cbc
Luca Fabbozzo <lucafabbozzo@gmail.com>
2022-06-04 09:33:45 -0500

    styling products

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,23 @@
 import { Component } from "react";
 
+const styles = {
+  product: {
+    border: 'solid 1px #eee',
+    boxShadow: '0 5px 5px rgb(0, 0, 0, 0.1)',
+    width: '30%',
+    padding: '10px 15px',
+    borderRadius: '5px'
+  },
+  img: {
+    width: '100%',
+  }
+}
 class Product extends Component {
   render() {
     const { product } = this.props
     return (
-      <div>
-        <img alt={product.name} src={product.img} />
+      <div style={styles.product}>
+        <img style={styles.img} alt={product.name} src={product.img} />
         <h3>{product.name}</h3>
         <p>{product.price}</p>
       </div>
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,12 +1,19 @@
 import { Component } from "react";
 import Product from './Product'
 
+const styles = {
+  products: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  }
+}
 class Products extends Component {
   render() {
     const { products, addToCart } = this.props
 
     return(
-      <div>
+      <div style={styles.products}>
         {products.map(product => 
           <Product
             addToCart={addToCart}
