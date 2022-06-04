95122c2a2dc8738000565e19171c63fa35b9c13f
Luca Fabbozzo <lucafabbozzo@gmail.com>
2022-06-04 10:33:06 -0500

    nav component

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import { Component } from "react";
+
+const styles = {
+  navbar: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: '100px',
+    justifyContent: 'space-between',
+    position: 'relative',
+    padding: '0 50px',
+    boxShadow: '0 2px 3px rgb(0, 0, 0, 0.1)'
+  }
+}
+
+class Navbar extends Component {
+  render() {
+    return(
+      <nav style={styles.navbar}>
+        <p>Logo</p>
+        <p>Cart</p>
+      </nav>
+    )
+  }
+}
+
+export default Navbar
