987d03444c0ce86190bae2438c2eec3db4d77f92
Luca Fabbozzo <lucafabbozzo@gmail.com>
2022-06-04 10:20:10 -0500

    title component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import Products from './components/Products'
 import Layout from './components/Layout'
+import Title from './components/Title'
 
 class App extends Component {
   state = {
@@ -14,6 +15,7 @@ class App extends Component {
     return(
       <div>
         <Layout>
+          <Title />
           <Products 
           addToCart={() => console.log('does nothing')}
           products={this.state.products}
