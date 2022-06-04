2af2dc0433dc75921e08ec850f44b55cb8c95cdc
Luca Fabbozzo <lucafabbozzo@gmail.com>
2022-06-04 08:50:49 -0500

    starting dev the application

---
+++ README.md
@@ -0,0 +1,4 @@
+In this Repo we build a Shopping-Cart - React class based components, this is taken from <a href="https://www.nicolas-schurmann.com/">Nicolas Shurmann<a> React course.
+
+
+<img src="./images/shopping-cart.jpg" />
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <p>Hola mundo</p>
+      </div>
+    )
+  }
+}
+
+export default App;
