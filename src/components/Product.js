b5667d9ef7f9524c9ff9b0923bfadd043d27b7f4
Luca Fabbozzo <lucafabbozzo@gmail.com>
2022-06-04 09:48:00 -0500

    add the cart button

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import Button from './Button'
 
 const styles = {
   product: {
@@ -14,12 +15,15 @@ const styles = {
 }
 class Product extends Component {
   render() {
-    const { product } = this.props
+    const { product, addToCart } = this.props
     return (
       <div style={styles.product}>
         <img style={styles.img} alt={product.name} src={product.img} />
         <h3>{product.name}</h3>
         <p>{product.price}</p>
+        <Button onClick={() => addToCart(product)}>
+          Add To Cart
+        </Button>
       </div>
     )
   }
